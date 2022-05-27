538d0b464bde31ef1a7ee9ff34b1dfab6ab4adb6
Mendy <mendyw94@gmail.com>
2022-05-27 02:07:56 -0400

    commit tree ef123150fbcddca1dbd15e76f4a7211737666470

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Your code here
+function createEmployeeRecord (arr = [firstName, lastName, title, hourlyRate]) {
+    let employeeRecord =  {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [ ],
+        timeOutEvents: [ ]
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords (arr) {
+    let employeeRecords = []
+        arr.forEach(employeeRec => {
+            employeeRecords.push(createEmployeeRecord(employeeRec))
+        })
+
+    
+    return employeeRecords
+}
+
+function createTimeInEvent (employeeRec, timeStamp) {
+    let dateNhour = timeStamp.split(' ')
+    employeeRec.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(dateNhour[1], 10),
+        date: `${dateNhour[0]}`
+    })
+    return employeeRec
+}
+
+function createTimeOutEvent (employeeRec, timeStamp) {
+    let dateNhour = timeStamp.split(' ')
+    let hourIn = parseInt(dateNhour[1], 10)
+    employeeRec.timeOutEvents.push(
+        {
+            type: 'TimeOut',
+            hour: hourIn,
+            date: `${dateNhour[0]}`
+        }
+    )
+    return employeeRec
+
+}
+
+function hoursWorkedOnDate (employeeObj, date) {
+    if (employeeObj.timeInEvents[0].date === date) {
+        return (employeeObj.timeOutEvents[0].hour - employeeObj.timeInEvents[0].hour) / 100
+    }
+}
+
+function wagesEarnedOnDate (employeeObj, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeObj, date)
+    return hoursWorked * employeeObj.payPerHour
+}
+
+function allWagesFor (employeeObj) {
+    let pay = 0;
+    for (let days in employeeObj.timeInEvents) {
+        pay += wagesEarnedOnDate(employeeObj, employeeObj.timeInEvents[days].date)
+    }
+    return pay
+}
+
+function calculatePayroll (arrOfemployees) {
+    let totalPay = 0;
+    for (let person of arrOfemployees) {
+        totalPay += allWagesFor(arrOfemployees[person])
+    }
+    return totalPay
+}
